2261805a4f394e1c3cf517d767904c95e89776e3
ercr07001 <ercr07001@E-PW05CGR0>
2024-10-10 12:51:22 +0200

    Add project files.

---
+++ Uppgift_2.4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in Eriks, Hugos och Victors lön här:");
+           
+            Console.WriteLine("Erik:");
+            Double Erik = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Hugo:");
+            Double Hugo = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Victor:");
+            Double Victor = Double.Parse(Console.ReadLine());
+
+            Double sum = Erik + Hugo + Victor;
+            Double sum1 = sum / 3;
+
+            Console.WriteLine("Medellönen är:" + " " + sum1 + " " + "med Eriks, Hugos och Victors lön");
+
+            Console.ReadKey();
+        }
+    }
+}
